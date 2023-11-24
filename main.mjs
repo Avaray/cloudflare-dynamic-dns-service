92ec108a0c7319cbbc6f84708261f5d12494fa53
Avaray <deffqqq@gmail.com>
2023-11-24 12:25:43 +0100

    commit przed refaktoryzacją
    ten i pierwszy commit pisałem na spontanie. teraz musze pomyslec nad logika...

--- main.mjs
+++ main.mjs
@@ -1,6 +1,6 @@
 import gip from 'gip';
 
-let ip, id;
+let ip, ipLastCheck, id;
 
 const subdomain = 'go.dav.one'
 
@@ -10,6 +10,7 @@ const authKey = 'ada47c6dcb7e14b593e380ddd434222131b53'
 const zoneID = '501d9018cb3c1f9cb85760ec8107984c'
 const apiUrlGetIds = `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records`;
 const apiUrlUpdateId = id =>  `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records/${id}`;
+const bodyUpdateId = () => ({ 'content': ip, 'name': subdomain, 'proxied': false, 'type': 'A', 'comment': 'Updated IP', 'tags': [], 'ttl': 60 })
 
 const options = (method) => ({
   method: String(method).toUpperCase(),
@@ -18,20 +19,29 @@ const options = (method) => ({
     'X-Auth-Email': authEmail,
     'X-Auth-Key': authKey,
   },
-  ...(method === 'PUT' && { body: {'content':ip, 'name':'go', 'proxied':false, 'type':'A', 'comment':'Updated IP', 'tags':[], 'ttl':60} })
+  ...(method === 'PUT' && { 'body': bodyUpdateId() })
 })
 
-const getID = async () => {
+// Find ID in record list
+const findID = async () => {
   const response = await fetch(apiUrlGetIds, options('GET'));
   return response.json();
 };
 
+// This function updates record with specific ID
 const updateID = async (id) => {
   const response = await fetch(apiUrlUpdateId(id), options('PUT'));
   return response.json();
 }
+
+// This function will create subdomain
+const createRecord = async (id) => {
+  const response = await fetch(apiUrlUpdateId(id), options('PUT'));
+  return response.json();
+}
+
 try {
-  const subdomains = await getID();
+  const subdomains = await findID();
   id = subdomains.result.find(entry => entry.name === subdomain).id;
   if (!id) throw new Error('ID not found');
   console.log(`Found ID ${id}`);
@@ -44,4 +54,5 @@ console.log(`Trying to update record`);
 
 const response = await updateID(id);
 
+console.log(response);
 response.success && console.log(`Subdomain ${subdomain} leads now to IP ${response.result.content}`);
