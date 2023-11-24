c255212496fc1c613749295b7bb9dbf33c627cb4
Avaray <deffqqq@gmail.com>
2023-11-24 20:54:45 +0100

    na dzis chyba koniec

--- main.mjs
+++ main.mjs
@@ -1,58 +1,91 @@
+// https://www.npmjs.com/package/gip
 import gip from 'gip';
 
-let ip, ipLastCheck, id;
+import Package from './package.json' assert { type: "json" };
+import Config from './config.json' assert { type: "json" };
 
-const subdomain = 'go.dav.one'
+const config = {
+  ip: '',
+  ipLastCheck: 0,
+  ipCheckIsRunning: false,
+  ...Config
+}
 
-const authEmail = 'deffqqq@gmail.com'
-const authKey = 'ada47c6dcb7e14b593e380ddd434222131b53'
+config.subdomains = {}
 
-const zoneID = '501d9018cb3c1f9cb85760ec8107984c'
-const apiUrlGetIds = `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records`;
-const apiUrlUpdateId = id =>  `https://api.cloudflare.com/client/v4/zones/${zoneID}/dns_records/${id}`;
-const bodyUpdateId = () => ({ 'content': ip, 'name': subdomain, 'proxied': false, 'type': 'A', 'comment': 'Updated IP', 'tags': [], 'ttl': 60 })
+Config.subdomains.forEach(subdomain => {
+  config.subdomains[`${subdomain}.${config.domain}`] = {
+    ip: '',
+    id: '',
+    updateIsRunning: false,
+  }
+})
 
-const options = (method) => ({
+// console.log(config);
+
+const apiUrlGetDnsRecords = `https://api.cloudflare.com/client/v4/zones/${config.zoneId}/dns_records`;
+const apiUrlUpdateRecord = (recordId) =>  `https://api.cloudflare.com/client/v4/zones/${config.zoneId}/dns_records/${recordId}`;
+const apiUrlAddRecord = `https://api.cloudflare.com/client/v4/zones/${config.zoneId}/dns_records`;
+const bodyUpdateId = (name) => ({ 'content': config.ip, 'name': name, 'proxied': false, 'type': 'A', 'comment': 'Updated IP', 'tags': [], 'ttl': config.ttl })
+const headersToken = { 'Authorization': `Bearer ${config.key}` }
+const headersGlobalKey = { 'X-Auth-Email': config.email, 'X-Auth-Key': config.key }
+
+const fetchOptions = (method = 'GET', name = '') => ({
   method: String(method).toUpperCase(),
   headers: {
     'Content-Type': 'application/json',
-    'X-Auth-Email': authEmail,
-    'X-Auth-Key': authKey,
+    ...(config.keyType === 'token' && headersToken),
+    ...(config.keyType === 'key' && headersGlobalKey),
   },
-  ...(method === 'PUT' && { 'body': bodyUpdateId() })
+  ...(method === 'PUT' && { body: bodyUpdateId(name) })
 })
 
-// Find ID in record list
-const findID = async () => {
-  const response = await fetch(apiUrlGetIds, options('GET'));
-  return response.json();
+const getDnsRecords = async () => {
+  const response = await fetch(apiUrlGetDnsRecords, fetchOptions('GET'));
+  return await response.json();
 };
+
+console.log(`Starting Cloudflare Dynamic DNS Service (version ${Package.version})`);
+
+console.log(`Trying to get existing DNS records`);
+
+process.exit()
+
+console.log(`Looking for existing DNS records`);
+
+Object.keys(config.subdomains).forEach(subdomain => {
+  const match = dnsRecordsFound.result.find(record => record.name === subdomain);
+  if (match) {
+    console.log(`Found ${subdomain} in DNS records`);
+    config.subdomains[subdomain].id = match.id;
+    config.subdomains[subdomain].ip = match.content;
+  }
+})
+
+// This function will create DNS record for subdomain
+const createRecord = async (name) => {
+  console.log(`Trying to create DNS record for ${name}`);
+  const response = await fetch(apiUrlAddRecord, fetchOptions('PUT', name));
+  console.log(await response.json());
+  // return response.json();
+}
+
+Object.keys(config.subdomains).forEach(async (subdomain) => {
+  if (!config.subdomains[subdomain].id) {
+    console.log(`Subdomain ${subdomain} not found in DNS records`);
+    const response = await createRecord(subdomain);
+    // console.log(response);
+  }
+})
+
+
+process.exit()
 
 // This function updates record with specific ID
 const updateID = async (id) => {
-  const response = await fetch(apiUrlUpdateId(id), options('PUT'));
+  const response = await fetch(apiUrlUpdateRecord(id), fetchOptions('PUT'));
   return response.json();
 }
 
-// This function will create subdomain
-const createRecord = async (id) => {
-  const response = await fetch(apiUrlUpdateId(id), options('PUT'));
-  return response.json();
-}
-
-try {
-  const subdomains = await findID();
-  id = subdomains.result.find(entry => entry.name === subdomain).id;
-  if (!id) throw new Error('ID not found');
-  console.log(`Found ID ${id}`);
-} catch (error) {
-  console.error(error);
-  process.exit(1);
-}
-
-console.log(`Trying to update record`);
-
-const response = await updateID(id);
-
-console.log(response);
-response.success && console.log(`Subdomain ${subdomain} leads now to IP ${response.result.content}`);
+// Calculate time from last IP check
+const ipCheckTimeDiff = (date1 = 0, date2 = Date.now()) => Math.floor(Math.abs(date1 - date2) / 1000);
